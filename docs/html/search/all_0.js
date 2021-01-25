72c9549c893f4e7c1321e682cd1223621f7bef95
gsvboas <gsvboas@estudante.ufscar.br>
2021-01-25 10:43:34 -0300

    docs output

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sbcreatewindow_0',['sbCreateWindow',['../sbWindow_8h.html#a6c0e974c30d4c9d8a5b1e7cf1b71817c',1,'sbWindow.h']]],
+  ['sbwindow_2eh_1',['sbWindow.h',['../sbWindow_8h.html',1,'']]]
+];
---
+++ docs/html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sbwindow_2eh_2',['sbWindow.h',['../sbWindow_8h.html',1,'']]]
+];
